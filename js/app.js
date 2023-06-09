7a912c0a512ba002333b544ebb2d5419d2ea94b9
thewa1818 <zawa.i.0218@gmail.com>
2023-06-09 17:58:16 +0900

    kadai

--- js/app.js
+++ js/app.js
@@ -1,29 +1,55 @@
 
+$(".closeBtn").on("click",function(){
+   $(".header_list").slideToggle(1000);
+   $(".closeBtn").fadeOut(1000);
+   $(".closeBtn2").fadeIn(1000);
+})
 
-$("h1").on("click", function () {
-  $(".a").html("押された");
+$(".closeBtn2").on("click",function(){
+  $(".header_list").slideToggle(1000);
+  $(".closeBtn2").fadeOut(1000);
+  $(".closeBtn").fadeIn(1000);
+})
 
-  const random = Math.floor(Math.random() * 5);
 
-  if (random === 0){
-    console.log("大吉です");
-    $(".a").html("大吉です");
-    $(".a").css("color","red");
-  }else if (random === 1){
-    console.log("中吉です");
-    $(".a").html("中吉です");
-    $(".a").css("color","black");
-  }else if (random === 2){
-    console.log("吉です");
-    $(".a").html("吉です");
-    $(".a").css("color","black");
-  }else if (random === 3){
-    console.log("小吉です");
-    $(".a").html("小吉です");
-    $(".a").css("color","black");
-  }else if (random === 4){
-    console.log("末です");
-    $(".a").html("末です");
-    $(".a").css("color","black");
-  }
-});
+
+$(".header_list-item").hover(function(){
+  $(this).css("background-color","#d4d1d1");
+},
+function(){
+  $(this).css("background-color","#fff");
+})
+
+$('header_item').scrollTop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".omikuji_title").on("click", function () {
+//   $(".omikuji_kekka").fadeIn();
+
+//   const random = Math.floor(Math.random() * 5);
+
+//   if (random === 0){
+//     console.log("大吉です");
+//   }else if (random === 1){
+//     console.log("中吉です");
+//   }else if (random === 2){
+//     console.log("吉です");
+//   }else if (random === 3){
+//     console.log("小吉です");
+//   }else if (random === 4){
+//     console.log("末です");
+//   }
+// });
